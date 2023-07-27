efcf47801502e20609b64163e1c24374d96de4e7
Russell Waterhouse <russell@ruboss.com>
2023-07-27 11:54:56 -0700

    Create basic Linked List implementation

--- data-structures/linked-list/haskell/linked-list.hs
+++ data-structures/linked-list/haskell/linked-list.hs
@@ -1,6 +1,17 @@
 
 
+data Node = Node 
+    { val :: Int
+    , next :: Node
+    } | EmptyNode deriving (Show, Eq)
+
+
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    let n1 = EmptyNode
+    print n1
+    let n2 = Node 10 n1
+    print n2
 
 
