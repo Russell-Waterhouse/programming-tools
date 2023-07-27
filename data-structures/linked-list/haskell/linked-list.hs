2fe069348fc3ae4bc97068fd03c241154b8aff22
Russell Waterhouse <russell@ruboss.com>
2023-07-27 12:12:07 -0700

    add insert to haskell linked list

--- data-structures/linked-list/haskell/linked-list.hs
+++ data-structures/linked-list/haskell/linked-list.hs
@@ -6,6 +6,9 @@ data Node = Node
     } | EmptyNode deriving (Show, Eq)
 
 
+insert :: Node -> Int -> Node
+insert EmptyNode value = Node value EmptyNode
+insert head value = Node (val head) (insert (next head) value)
 
 main :: IO ()
 main = do
@@ -13,5 +16,8 @@ main = do
     print n1
     let n2 = Node 10 n1
     print n2
+
+    let n3 = insert n2 20
+    print n3
 
 
