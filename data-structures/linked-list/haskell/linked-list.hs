4f521a0f315869f5f75d2e534f914afd5aaa61a0
Russell Waterhouse <russell@ruboss.com>
2023-07-31 21:09:24 -0700

    add functionality to haskell linked list implementation

--- data-structures/linked-list/haskell/linked-list.hs
+++ data-structures/linked-list/haskell/linked-list.hs
@@ -6,9 +6,28 @@ data Node = Node
     } | EmptyNode deriving (Show, Eq)
 
 
-insert :: Node -> Int -> Node
-insert EmptyNode value = Node value EmptyNode
-insert head value = Node (val head) (insert (next head) value)
+tailInsert :: Node -> Int -> Node
+tailInsert EmptyNode value = Node value EmptyNode
+tailInsert head value = Node (val head) (tailInsert (next head) value)
+
+
+headInsert :: Node -> Int -> Node
+headInsert head value = Node value head
+
+
+printList :: Node -> IO ()
+printList = do print 
+
+
+purgeValue :: Node -> Int -> Node
+purgeValue EmptyNode _ = EmptyNode
+purgeValue head value = let
+        headIsValue = value == val head
+    in
+        if headIsValue then
+            purgeValue (next head) value
+        else
+            Node (val head) $ purgeValue (next head) value
 
 main :: IO ()
 main = do
@@ -17,7 +36,12 @@ main = do
     let n2 = Node 10 n1
     print n2
 
-    let n3 = insert n2 20
-    print n3
+    let n3 = tailInsert n2 20
+    printList n3
+    
+    let n4 = headInsert n3 5
+    printList n4
 
+    let n5 = purgeValue n4 10
+    printList n5
 
