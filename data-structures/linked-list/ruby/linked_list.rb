1bb6244b0cb44589fc9f6539a06de20fc9543775
Russell Waterhouse <russell@ruboss.com>
2023-08-02 15:16:31 -0700

    start ruby linked list implementation

---
+++ data-structures/linked-list/ruby/linked_list.rb
@@ -0,0 +1,26 @@
+class LinkedList
+
+  @value
+  @next
+
+  def insert_head(head, val)
+    new_head = LinkedList(value: val, next: head)
+    return new_head
+  end
+
+  def insert_tail()
+    # todo
+  end
+
+  def delete_node()
+    # todo
+  end
+
+  def size()
+    # todo
+  end
+
+  def print_list()
+    # todo
+  end
+end
---
+++ data-structures/linked-list/ruby/test.rb
@@ -0,0 +1,4 @@
+
+puts "Test started"
+puts "Test finished"
+
