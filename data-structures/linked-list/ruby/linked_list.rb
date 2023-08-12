e7b4170f1fde836e560f3c1db9d179dcbebae1d2
Russell Waterhouse <russell@ruboss.com>
2023-08-12 10:35:14 -0700

    finish implementing ruby linked list

--- data-structures/linked-list/ruby/linked_list.rb
+++ data-structures/linked-list/ruby/linked_list.rb
@@ -10,28 +10,59 @@ class LinkedList
     @head = nil
   end
 
-  def insert_head(head, val)
-    new_head = Node.new(value: val, next_node: head)
+  def insert_head(val)
+    new_head = Node.new(value: val, next_node: @head)
     @head = new_head
+    @size += 1
+    self
   end
 
-  def insert_tail()
-    # todo
+  def insert_tail(val)
+    if @head == nil
+      @head = Node.new(value: val, next_node: nil)
+      return
+    end
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    current.set_next Node.new(value: val, next_node: nil)
   end
 
-  def delete_node()
-    # todo
+  def delete_node(node)
+    if @head == node
+      @head = node.next
+      return
+    end
+
+    current = @head
+    while current.next != node
+      current = current.next
+    end
+    if current.next != node
+      puts "not found"
+      return
+    end
+    current.set_next current.next.next
+    @size -= 1
+    return
   end
 
   def size()
-    # todo
+    @size
   end
 
-  def print_list()
+  def to_string()
     current = @head
+    list_str = ""
     while current != nil do
-      puts "Node: #{current.val}"
+      list_str += "Node: #{current.val} -> "
       current = current.next
     end
+    list_str
+  end
+
+  def head()
+    @head
   end
 end
--- data-structures/linked-list/ruby/test.rb
+++ data-structures/linked-list/ruby/test.rb
@@ -1,15 +1,77 @@
 require_relative "linked_list"
+require_relative "node"
 
 
 
-puts "Test started"
-puts "Test insert"
-l = LinkedList.new()
+def test_insert_head()
+  ll = LinkedList.new
+  ll.insert_head(2)
+  ll.insert_head(1)
+  result = ll.to_string
+  expected = "Node: 1 -> Node: 2 -> "
+  if result == expected
+    puts "Test Insert Head Passed"
+  else
+    puts "Test Insert Head Failed"
+  end
+end
 
-l.print_list
+def test_tail_insert()
+  ll = LinkedList.new
+  ll.insert_tail(2)
+  ll.insert_tail(1)
+  result = ll.to_string
+  expected = "Node: 2 -> Node: 1 -> "
+  if result == expected
+    puts "Test Insert Tail Passed"
+  else
+    puts "Test Insert Tail Failed"
+  end
+end
 
-l.insert_head(nil, 10)
-l.print_list
+def test_delete_node()
+  ll = LinkedList.new
+  ll.insert_head(1)
+  ll.insert_tail(2)
+  ll.insert_tail(3)
 
-puts "Test finished"
+  n1 = ll.head
+  n2 = n1.next
+  n3 = n2.next
+  ll.delete_node(n2)
+  expected1 = "Node: 1 -> Node: 3 -> "
+  result1 = ll.to_string
+
+  ll.delete_node(n1)
+  expected2 = "Node: 3 -> "
+  result2 = ll.to_string
+
+  if result1 == expected1 && result2 == expected2
+    puts "Test Delete Node Passed"
+  else
+    puts "Test Delete Node Failed"
+  end
+end
+
+def test_print_list()
+  ll = LinkedList.new
+  ll.insert_head(4)
+  ll.insert_head(3)
+  ll.insert_head(2)
+  ll.insert_head(1)
+  ll.insert_head(0)
+  ll.insert_head(-1)
+  expected = "Node: -1 -> Node: 0 -> Node: 1 -> Node: 2 -> Node: 3 -> Node: 4 -> "
+  result = ll.to_string
+  if result == expected
+    puts "Test Print List Passed"
+  else
+    puts "Test Print List Failed"
+  end
+end
+
+test_insert_head()
+test_tail_insert()
+test_delete_node()
+test_print_list()
 
