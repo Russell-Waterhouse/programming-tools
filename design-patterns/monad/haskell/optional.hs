11866d9c27e3468fdb0f5b7a59bed963b12908d4
Russell Waterhouse <russell@ruboss.com>
2023-08-15 20:16:42 -0700

    add function to optional

--- design-patterns/monad/haskell/optional.hs
+++ design-patterns/monad/haskell/optional.hs
@@ -4,6 +4,10 @@ import MyMonad (MyMonad, bind, myReturn)
 
 data Optional a = Empty | Full a 
 
+isEmpty :: Optional -> Bool
+isEmpty Empty = True
+isEmpty _ = False
+
 
 instance MyMonad Optional where
     bind Empty _ = Empty
