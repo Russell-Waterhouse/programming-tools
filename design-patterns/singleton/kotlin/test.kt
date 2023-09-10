d1c0151c77046be07162cdb9cda7efcd015ff6b9
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2023-09-10 13:46:59 -0700

    finish Kotlin Singleton implementation

--- design-patterns/singleton/kotlin/test.kt
+++ design-patterns/singleton/kotlin/test.kt
@@ -1,4 +1,20 @@
-// Kotlin Hello World Program
+fun testSingletonEquality(){
+    val singletonId = 10
+    Singleton.id = singletonId
+    val logicalEquivalent = Singleton.equals(Singleton)
+    val objectEquivalent = Singleton == Singleton
+    val idEquivalent = Singleton.id == singletonId
+    if (
+        logicalEquivalent &&
+        objectEquivalent &&
+        idEquivalent
+    ) {
+        println("Test Singleton: Passed ")
+    } else {
+        println("Test Singleton: Failed")
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello, World!")
+    testSingletonEquality()
 }
